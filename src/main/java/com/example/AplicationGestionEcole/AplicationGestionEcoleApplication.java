df13f35545e9bcf4b5d0d344cda788d166c60518
SamiMas24 <samirahmasimina@gmail.com>
2022-06-05 19:40:58 +0200

    projet

---
+++ src/main/java/com/example/AplicationGestionEcole/AplicationGestionEcoleApplication.java
@@ -0,0 +1,13 @@
+package com.example.AplicationGestionEcole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicationGestionEcoleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicationGestionEcoleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/AplicationGestionEcole/AplicationGestionEcoleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AplicationGestionEcole;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplicationGestionEcoleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
